60557f4ed12c5f47bc0a243306e73e1f2e385ac2
hleu0019 <ryanleung1029@gmail.com>
2023-05-03 00:50:32 +1000

    connect to league api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,26 @@
-export default function Home() {
+export async function getServerSideProps() {
+  const requestHeaders: HeadersInit = new Headers();
+  requestHeaders.append("X-Riot-Token", process.env.API_KEY!);
+
+  const data = await fetch(
+    "https://oc1.api.riotgames.com/lol/summoner/v4/summoners/by-name/ShortMaker",
+    {
+      headers: requestHeaders,
+    }
+  ).then((x) => x.json());
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+}
+
+interface HomeProps {
+  data: JSON;
+}
+
+export default function Home({ data }: HomeProps) {
   
-  return <div className="text-red-400">Hello</div>
+  return <div className="text-red-400">{JSON.stringify(data)}</div>;
 }
